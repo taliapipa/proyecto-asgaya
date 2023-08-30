c1bdb6a4e27426b1b6d96828e6a4e01221a53439
taliapipa <125282306+taliapipa@users.noreply.github.com>
2023-08-30 12:07:34 +0200

    footer estilos

--- asgaya2/src/Componentes/Core/Footer/Footer.jsx
+++ asgaya2/src/Componentes/Core/Footer/Footer.jsx
@@ -1,10 +1,41 @@
 import '../Footer/Footer.css'
+import imgLogo from '../../../Assets/LOGOTIPOlogo-1.svg'
+import Button from '../../Shared/Button/button'
 
 const Footer = ()=> {
     return (
-        <div>
-            <h1>FOOTER WORKS!</h1>
-        </div>
+        <footer className='footer'>
+            <div className='logo'>
+                <img src={imgLogo} alt="logo-asgaya" className="imgLogo"/>
+            </div>
+            <div className='enlaces'>
+                <div className='bloque empresa'>
+                    <p className='titulo'>Empresa</p>
+                    <p>Quienes somos</p>
+                    <p>Contacto</p>
+                    <p>Blog</p>
+                </div>
+                <div className='bloque legal'>
+                    <p className='titulo'>Legal</p>
+                    <p>Términos y condiciones</p>
+                    <p>Política de privacidad</p>
+                    <p>Política de cookies</p>
+                    <p>Aviso legal</p>
+                </div>
+                <div className='bloque newsletter'>
+                    <p className='titulo'>Suscríbete a nuestra Newsletter!</p>
+                    <form className='suscribe'>
+                        <input type="text" id="email" name="email" placeholder="Email"/>
+                        <Button text="Enviar" value="Enviar" type="submit"/>
+                    </form>
+                    <div className='div-checkbox'>
+                        <input type='checkbox' className='checkbox'></input>
+                        <p className='acepto'>Acepto la política de privacidad y recibir las comunicaciones comerciales de Asgaya. Datos tratados para el envío de comunicaciones comerciales personalizadas a tus gustos e intereses. Puedes ejercer los derechos de acceso, rectificación, supresión, oposición y otros.</p>
+                    </div>
+
+                </div>
+            </div>
+        </footer>
     )
 }
 export default Footer 
