83500f25cdd7315796732e2544c58ce520b0dff5
taliapipa <125282306+taliapipa@users.noreply.github.com>
2023-09-13 19:41:28 +0200

    quienes somos

--- asgaya2/src/Componentes/Pages/About.jsx
+++ asgaya2/src/Componentes/Pages/About.jsx
@@ -1,6 +1,8 @@
 import './About.css'
 import AboutBanner from "../../Assets/sobrenosotrosbanner.png"
 import Gallinas from "../../Assets/gallinas.png"
+import Button from '../Shared/Button/button'
+
 const About = ()=>{
     return (
         <div>
@@ -8,8 +10,8 @@ const About = ()=>{
                 <img src={AboutBanner} alt='banner-about' className='banner-about-img'/>
                 <h1 className='title-about'>SOBRE NOSOTROS</h1>
             </div>
-            <div>
-                <div>
+            <div className='quienessomos-part'>
+                <div className='quienessomos-part-text'>
                     <h2>
                         QUIENES SOMOS
                     </h2>
@@ -21,9 +23,61 @@ const About = ()=>{
                         ¡Como tener una granja en casa!
                     </p>
                 </div>
-                <img src={Gallinas} alt='gallinas' className='gallinas-quienessomos'/>
+                <div className='gallinas-quienessomos'>
+                    <img src={Gallinas} alt='gallinas' />
+                </div>
+            </div>
+
+            <div className='mision-part'>
+                <div className='cerdos-quienessomos'>
+                    <img src={Gallinas} alt='cerdos' />
+                </div>
+                <div className='mision-part-text'>
+                    <h2>
+                        MISIÓN
+                    </h2>
+                    <p>
+                        Creemos que dar el medio natural a aquellas personas que no pueden crear su propia granja, ya bien porque vivan en la ciudad o no tengan los medios para poder, pero siempre con conciencia y ética. Por eso creamos la idea de una granja desde casa con los productos naturales como si hubieses salido al campo a recogerlos.
+                    </p>
+                </div>
 
             </div>
+
+            <div className='quienessomos-part'>
+                <div className='quienessomos-part-text'>
+                    <h2>
+                        VISIÓN
+                    </h2>
+                    <p>
+                        Proporcionar acceso a productos 100% naturales y directamente desde la granja con un solo click. 
+                    </p>
+                </div>
+                <div className='gallinas-quienessomos'>
+                    <img src={Gallinas} alt='gallinas' />
+                </div>
+            </div>
+
+            <div className='mision-part'>
+                <div className='cerdos-quienessomos'>
+                    <img src={Gallinas} alt='cerdos' />
+                </div>
+                <div className='mision-part-text'>
+                    <h2>
+                        VALORES
+                    </h2>
+                    <p>
+                        Eco-friendly, cultry-free, sostenibilidad, calidad e integridad.
+                    </p>
+                </div>
+
+            </div>
+
+            <div className='banner-blog'>
+                <h1>CONOCE MÁS SOBRE NOSOTROS EN NUESTRO BLOG</h1>
+                <Button text='Leer más' />
+            </div>
+
+            
         </div>
 
     )
